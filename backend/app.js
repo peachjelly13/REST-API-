ef56fc351d13dff94935a5c5877e1993e9b7d3ae
peachjelly13 <ananya.sharma2021a@vitstudent.ac.in>
2024-06-16 19:13:36 +0530

    connection

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+
+
+const app = express();
+//mongoose.connect() returns a promise
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{app.listen(5000);})
+.then(()=>{(console.log("Listening to the port 5000"))})
+.catch((err)=>{(console.log(err))})
